a55c79e8aac74dcc7445e62da334d688634a75bb
shahryar1989 <omidvari1989@gmail.com>
2016-05-28 20:26:54 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {95643536},--Sudo users
+    sudo_users = {shahryar1989},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
